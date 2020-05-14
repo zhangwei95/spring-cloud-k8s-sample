6b2d0e5e2dae2fc25420f82d866bd7f2d1363a17
zhangwei95 <574150903@qq.com>
2020-05-14 11:38:26 +0800

    version fix

--- spring-cloud-service-consumer/src/test/java/com/zw/springcloud/service/consumer/SpringCloudServiceConsumerApplicationTests.java
+++ spring-cloud-service-consumer/src/test/java/com/zw/springcloud/service/consumer/SpringCloudServiceConsumerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.zw.springcloud.service.consumer;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
