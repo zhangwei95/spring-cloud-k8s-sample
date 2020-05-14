c3f71f8fccf834b91ec07f9a5cecc794a72520a0
zhangwei95 <574150903@qq.com>
2020-05-14 11:15:03 +0800

    k8s 测试 服务注册和feign

--- spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/SpringCloudServiceProviderApplication.java
+++ spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/SpringCloudServiceProviderApplication.java
@@ -2,8 +2,10 @@ package com.zw.springcloudserviceprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SpringCloudServiceProviderApplication {
 
     public static void main(String[] args) {
