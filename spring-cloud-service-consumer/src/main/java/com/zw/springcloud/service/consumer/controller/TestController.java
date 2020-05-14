9b4db3f5a9a8d5484bee08fd094822c8094ad27c
zhangwei95 <574150903@qq.com>
2020-05-14 13:52:10 +0800

    log

--- spring-cloud-service-consumer/src/main/java/com/zw/springcloud/service/consumer/controller/TestController.java
+++ spring-cloud-service-consumer/src/main/java/com/zw/springcloud/service/consumer/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.zw.springcloud.service.consumer.controller;
 
 import com.zw.springcloud.service.consumer.client.ProviderClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class TestController {
 
     @Autowired
@@ -16,6 +18,7 @@ public class TestController {
 
     @GetMapping("/getService")
     public String getService(@RequestParam String name){
+        log.error("get service name{}",name);
         return providerClient.getService(name);
     }
 
--- spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/controller/TestController.java
+++ spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.zw.springcloudserviceprovider.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class TestController {
 
     @GetMapping("/getService")
     public String getService(@RequestParam String name){
+        log.info("provider get service {}",name);
         return name;
     }
 
