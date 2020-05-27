e097d8b73809b1644ae7930ee8505832cf503357
zhangwei95 <574150903@qq.com>
2020-05-27 17:08:10 +0800

    safe shutdown

--- spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/controller/TestController.java
+++ spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.zw.springcloudserviceprovider.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,10 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
+    @Autowired
+    private ApplicationContext context;
+
     @GetMapping("/getService")
     public String getService(@RequestParam String name){
         log.info("provider get service {}",name);
         return name;
+    }
+
+
+    @GetMapping("/sleep")
+    public String sleep() throws InterruptedException {
+        log.error("sleep start");
+        Thread.sleep(30000);
+        log.error("sleep end");
+        return "wakeup";
+    }
+
+
+    @GetMapping("/systemOut")
+    public void systemOut() {
+        ConfigurableApplicationContext ctx = (ConfigurableApplicationContext) context;
+        ctx.close();
     }
 
 }
