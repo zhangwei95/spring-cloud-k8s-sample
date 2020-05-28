5abf26ec35ce369d31a611619f780762bc04cede
zhangwei95 <574150903@qq.com>
2020-05-28 10:12:20 +0800

    undertow safe shutdown

--- spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/GracefulShutdownUndertowWrapper.java
+++ spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/GracefulShutdownUndertowWrapper.java
@@ -1,26 +1,26 @@
-//package com.zw.springcloudserviceprovider;
-//
-//import io.undertow.server.HandlerWrapper;
-//import io.undertow.server.HttpHandler;
-//import io.undertow.server.handlers.GracefulShutdownHandler;
-//import org.springframework.stereotype.Component;
-//
-//
-///**
-// * @Author: guoyankui
-// * @DATE: 2018/5/20 5:50 PM
-// */
-//@Component
-//public class GracefulShutdownUndertowWrapper implements HandlerWrapper {
-//    private GracefulShutdownHandler gracefulShutdownHandler;
-//    @Override
-//    public HttpHandler wrap(HttpHandler handler) {
-//        if(gracefulShutdownHandler == null) {
-//            this.gracefulShutdownHandler = new GracefulShutdownHandler(handler);
-//        }
-//        return gracefulShutdownHandler;
-//    }
-//    public GracefulShutdownHandler getGracefulShutdownHandler() {
-//        return gracefulShutdownHandler;
-//    }
-//}
+package com.zw.springcloudserviceprovider;
+
+import io.undertow.server.HandlerWrapper;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.handlers.GracefulShutdownHandler;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @Author: guoyankui
+ * @DATE: 2018/5/20 5:50 PM
+ */
+@Component
+public class GracefulShutdownUndertowWrapper implements HandlerWrapper {
+    private GracefulShutdownHandler gracefulShutdownHandler;
+    @Override
+    public HttpHandler wrap(HttpHandler handler) {
+        if(gracefulShutdownHandler == null) {
+            this.gracefulShutdownHandler = new GracefulShutdownHandler(handler);
+        }
+        return gracefulShutdownHandler;
+    }
+    public GracefulShutdownHandler getGracefulShutdownHandler() {
+        return gracefulShutdownHandler;
+    }
+}
