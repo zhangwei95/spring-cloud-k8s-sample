4e754ee073f9502c32ca68da0b17d167fd53308c
zhangwei95 <574150903@qq.com>
2020-05-28 10:54:27 +0800

    print activityConnections

--- spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/GracefulShutdownUndertow.java
+++ spring-cloud-service-provider/src/main/java/com/zw/springcloudserviceprovider/GracefulShutdownUndertow.java
@@ -2,8 +2,12 @@ package com.zw.springcloudserviceprovider;
 
 
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import io.undertow.Undertow;
 import io.undertow.server.ConnectorStatistics;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.embedded.undertow.UndertowServletWebServer;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
@@ -13,6 +17,8 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @Author: guoyankui
@@ -21,6 +27,7 @@ import java.util.List;
  * 优雅关闭 Spring Boot undertow
  */
 @Component
+@Slf4j
 public class GracefulShutdownUndertow  implements ApplicationListener<ContextClosedEvent> {
     @Autowired
     private GracefulShutdownUndertowWrapper gracefulShutdownUndertowWrapper;
@@ -28,6 +35,17 @@ public class GracefulShutdownUndertow  implements ApplicationListener<ContextClo
     @Autowired
     private ServletWebServerApplicationContext context;
 
+    LoadingCache<Long, AtomicLong> counter= CacheBuilder.newBuilder()
+            .expireAfterWrite(2, TimeUnit.SECONDS)
+            .build(new CacheLoader<Long, AtomicLong>() {
+                @Override
+                public AtomicLong load(Long aLong) throws Exception {
+                    return new AtomicLong(0);
+                }
+            });
+
+    int limit=2;
+    int unit=1000;
     @Override
     public void onApplicationEvent(ContextClosedEvent contextClosedEvent){
         gracefulShutdownUndertowWrapper.getGracefulShutdownHandler().shutdown();
@@ -39,7 +57,13 @@ public class GracefulShutdownUndertow  implements ApplicationListener<ContextClo
             List<Undertow.ListenerInfo> listenerInfo = undertow.getListenerInfo();
             Undertow.ListenerInfo listener = listenerInfo.get(0);
             ConnectorStatistics connectorStatistics = listener.getConnectorStatistics();
-            while (connectorStatistics.getActiveConnections() > 0){}
+            while (connectorStatistics.getActiveConnections() > 0){
+                Long current = System.currentTimeMillis()/unit;
+                if(counter.get(current).incrementAndGet()<limit){
+                    log.info("当前连接数："+connectorStatistics.getActiveConnections());
+                }
+            }
+            log.info("当前连接数："+connectorStatistics.getActiveConnections());
         } catch (Exception e){
             // Application Shutdown
         }
